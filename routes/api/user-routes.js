4de85c078f37b88b8d846292a3d52123bd26bfdd
jbairoso <jbairoso@gmail.com>
2022-06-13 19:55:44 -0700

    user login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -20,7 +20,7 @@ router.get("/:id", (req, res) => {
     attributes: { exclude: ["password"] },
     where: {
       id: req.params.id,
-    }
+    },
   })
     .then((dbUserData) => {
       if (!dbUserData) {
@@ -48,6 +48,30 @@ router.post("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+//create new user
+router.post("/login", (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
 });
 
 // PUT /api/users/1
